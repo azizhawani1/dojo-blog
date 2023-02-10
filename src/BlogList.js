8350c3ac3d80e4c00ada5482338844ff0744e690
Azizhawani <Azizhawani@MSI>
2023-02-10 15:50:44 +0100

    Add base url root for blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -13,7 +13,7 @@ const BlogList = ({ blogs, title, handeDelete}) => {
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <Link to={'/blogs/' + blog.id}>
+                    <Link to={'/dojo-blog/blogs/' + blog.id}>
                         <h2>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
                     </Link>
